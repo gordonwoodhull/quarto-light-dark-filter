76844a93d4cfbfc5795dcc93dfaf28831b76cb26
Gordon Woodhull <gordon.woodhull@posit.co>
2025-08-26 12:01:08 -0400

    try 3: add divs
    this would need to be more defensive but seems to work

--- add-dark-image.lua
+++ add-dark-image.lua
@@ -1,7 +1,8 @@
-function Image(img)
-  local darkimg = img:walk {}
-  img.attr.classes = {"light-content"}
-  darkimg.attr.classes = {"dark-content"}
-  darkimg.src = darkimg.src:gsub("%.png", "-dark.png")
-  return pandoc.Span({img, darkimg})
+function Figure(fig)
+  local darkfig = fig:walk {}
+  darkfig.content[1].content[1].src = darkfig.content[1].content[1].src:gsub("%.png", "-dark.png")
+  return {
+    pandoc.Div(fig, pandoc.Attr("", {"light-content"})),
+    pandoc.Div(darkfig, pandoc.Attr("", {"dark-content"}))
+  }
 end
