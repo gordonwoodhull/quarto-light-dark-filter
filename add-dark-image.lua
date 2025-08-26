c38da87d863b51635f44f2c8be378464dafc4665
Gordon Woodhull <gordon.woodhull@posit.co>
2025-08-26 13:43:55 -0400

    try 4: both filters, activated with two classes
    not pretty, perhaps someone else can find a way
    to reduce this to one class

--- add-dark-image.lua
+++ add-dark-image.lua
@@ -1,8 +1,24 @@
 function Figure(fig)
+  if not fig.content[1] or not fig.content[1].content[1] or
+    not fig.content[1].content[1].attr.classes:includes "dark-div" then
+    return nil
+  end
+  quarto.log.output(fig)
   local darkfig = fig:walk {}
   darkfig.content[1].content[1].src = darkfig.content[1].content[1].src:gsub("%.png", "-dark.png")
   return {
     pandoc.Div(fig, pandoc.Attr("", {"light-content"})),
     pandoc.Div(darkfig, pandoc.Attr("", {"dark-content"}))
   }
+end
+
+function Image(img)
+  if not img.src or not img.attr.classes:includes "dark-image" then
+    return nil
+  end
+  local darkimg = img:walk {}
+  img.attr.classes = {"light-content"}
+  darkimg.attr.classes = {"dark-content"}
+  darkimg.src = darkimg.src:gsub("%.png", "-dark.png")
+  return {img, darkimg}
 end
